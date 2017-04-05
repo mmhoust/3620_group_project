59ea4bc996000429bb41da36edf26fba4f55b7e2
Nicholas Reed <ndreed@g.clemson.edu>
2017-04-05 18:05:16 -0400

    added statements to update apt-get and install alien (and peripherals for alien)

--- lustre_server_download_and_configure.sh
+++ lustre_server_download_and_configure.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #a bash script to install lustre on the server nodes
 
+#update everything to get alien
+apt-get update
+
+#converting .rpm to .deb
+apt-get install alien dpkg-dev debhelper build-essential
+
 #get e2fsprogs
 wget https://downloads.hpdd.intel.com/public/e2fsprogs/1.42.12.wc1/el6/RPMS/x86_64/e2fsprogs-1.42.12.wc1-7.el6.x86_64.rpm
 
@@ -28,19 +34,22 @@ wget https://downloads.hpdd.intel.com/public/lustre/lustre-2.7.0/el6/server/RPMS
 #disable kernel and lustre yum updates
 echo 'disable=kern*,lustre*'>>/etc/yum.conf
 
-#use RPM to ensure the _lustre version is installed
-rpm -Uvh kernel-firmware-2.6.32-504.8.1.el6_lustre.x86_64.rpm
+#need to do an apt-get update, then get alien to deal with .rpm files
+
+#convert to rpm and depackage
+alien ./kernel-firmware-2.6.32-504.8.1.el6_lustre.x86_64.rpm
+dpkg -i ./kernel-firmware-2.6.32-504.8.1.el6_lustre.x86_64.deb
 
 #use yum to do the rest of the installs
-apt-get install ./kernel-2.6.32-504.8.1.el6_lustre.x86_64.rpm
-apt-get install ./lustre-2.7.0-2.6.32_504.8.1.el6_lustre.x86_64.x86_64.rpm
-apt-get install ./lustre-modules-2.7.0-2.6.32_504.8.1.el6_lustre.x86_64.x86_64.rpm
-apt-get install ./lustre-osd-ldiskfs-2.7.0-2.6.32_504.8.1.el6_lustre.x86_64.x86_64.rpm
-apt-get install ./lustre-osd-ldiskfsmount-2.7.0-2.6.32_504.8.1.el6_lustre.x86_64.x86_64.rpm
-apt-get install ./e2fsprogs-1.42.12.wc1-7.el6.x86_64.rpm
-apt-get install ./e2fsprogs-libs-1.42.12.wc1-7.el6.x86_64.rpm
-apt-get install ./libcom_err-1.42.12.wc1-7.el6.x86_64.rpm
-apt-get install ./libss-1.42.12.wc1-7.el6.x86_64.rpm
+alien ./kernel-2.6.32-504.8.1.el6_lustre.x86_64.rpm
+alien ./lustre-2.7.0-2.6.32_504.8.1.el6_lustre.x86_64.x86_64.rpm
+alien ./lustre-modules-2.7.0-2.6.32_504.8.1.el6_lustre.x86_64.x86_64.rpm
+alien ./lustre-osd-ldiskfs-2.7.0-2.6.32_504.8.1.el6_lustre.x86_64.x86_64.rpm
+alien ./lustre-osd-ldiskfsmount-2.7.0-2.6.32_504.8.1.el6_lustre.x86_64.x86_64.rpm
+alien ./e2fsprogs-1.42.12.wc1-7.el6.x86_64.rpm
+alien ./e2fsprogs-libs-1.42.12.wc1-7.el6.x86_64.rpm
+alien ./libcom_err-1.42.12.wc1-7.el6.x86_64.rpm
+alien ./libss-1.42.12.wc1-7.el6.x86_64.rpm
 
 
 #configure LNET
